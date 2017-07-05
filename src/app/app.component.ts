8a9a35ab2e3ceda1cde7924aa5c8465915ab6d10
Subrahmanya Satya Surendra Manepalli <E002832@cigniti.com>
2017-07-05 19:37:47 +0530

    firstcomit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  onSubmit(form:NgForm){
+    console.log(form);
+  }
 }
